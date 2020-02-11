d9129e7f60d33ff431aa30f660ea0db6935a0c18
micsoni <micsoni@gmail.com>
2020-02-11 15:57:02 +0100

    added css and 2 classes

---
+++ src/components/Thumbnails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Thumbnail extends React.Component {
+  render() {
+    return (
+      <div className="thumbnail">
+        <img className="image" src={this.props.src} />
+        <div className="fact-text">{this.props.text}</div>
+      </div>
+    );
+  }
+}
